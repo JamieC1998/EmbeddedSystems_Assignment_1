d28d538449d9a051fccfafcf20e503af2720f0d1
Jamie Cotter <jamie.cotter@mycit.ie>
2018-11-07 11:40:52 +0000

    Added comments

--- example-broadcast.c
+++ example-broadcast.c
@@ -9,36 +9,50 @@
 #include "dev/leds.h"
 
 #include <stdio.h>
+
+
 /*---------------------------------------------------------------------------*/
 PROCESS(example_broadcast_process, "Broadcast example");
 AUTOSTART_PROCESSES(&example_broadcast_process);
 /*---------------------------------------------------------------------------*/
 
-static void
-broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from)
-{
-  
+//This block of code executes when the collector receives a broadcast
+static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from){
+
 }
+
+//This struct lets the collector know what method to invoke when a broadcast is received
 static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
 static struct broadcast_conn broadcast;
 
+/*
+This block of code executes when the collector receives a unicast
+It prints out the average temperature received from what node
+*/
 static void recv_uc(struct unicast_conn *c, const rimeaddr_t *from) {
   printf("Unicast message received from %d.%d: '%s'\n",
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
 }
 
+//This struct lets the collector know what method to invoke when a unicast is received
 static const struct unicast_callbacks unicast_callbacks = { recv_uc };
 static struct unicast_conn uc;
 
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(example_broadcast_process, ev, data)
 {
+
+  //Creating our ETimer
   static struct etimer et;
 
   PROCESS_EXITHANDLER(broadcast_close(&broadcast);)
 
   PROCESS_BEGIN();
 
+  /*
+  Here we open a broadcast connection on band 146
+  and open a unicast on band 140
+  */
   broadcast_open(&broadcast, 146, &broadcast_call);
 
   unicast_open(&uc, 140, &unicast_callbacks);
@@ -50,6 +64,10 @@ PROCESS_THREAD(example_broadcast_process, ev, data)
 
     PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
 
+    /*
+    Every 2 - 4 seconds we copy a string into the packetbuffer
+    and then send out the packet in a broadcast
+    */
     packetbuf_copyfrom("Hello", 6);
     broadcast_send(&broadcast);
 
--- example-unicast.c
+++ example-unicast.c
@@ -15,24 +15,33 @@
 PROCESS(example_unicast_process, "Example unicast");
 AUTOSTART_PROCESSES(&example_unicast_process);
 /*---------------------------------------------------------------------------*/
-static void
-recv_uc(struct unicast_conn *c, const rimeaddr_t *from)
-{
-  printf("unicast message received from %d.%d\n",
-	 from->u8[0], from->u8[1]);
+
+/* This block of code executes when the collector receives a unicast */
+static void recv_uc(struct unicast_conn *c, const rimeaddr_t *from){
+
 }
+
+//This struct lets the collector know what method to invoke when a unicast is received
 static const struct unicast_callbacks unicast_callbacks = {recv_uc};
 static struct unicast_conn uc;
 
 /*---------------------------------------------------------------------------*/
 
-
+//This is the value that will be printed out when a broadcast is received
 static int print_val;
 
+//This array will be used to hold the moving average of the nodes temperature
 static int values[4];
 
-static void
-broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from){
+/* 
+This block of code is executed when a broadcast is received.
+It prints out the message that the node receives from the collector
+
+We convert our temperature value to a string and then copy it to the
+packet buffer.
+Then we send a unicast to the collector by
+setting our destination address to the two halves of the source address */
+static void broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from){
   
   printf("broadcast message received from %d.%d: '%s'\n",
          from->u8[0], from->u8[1], (char *)packetbuf_dataptr());
@@ -56,6 +65,7 @@ broadcast_recv(struct broadcast_conn *c, const rimeaddr_t *from){
 
 }
 
+//This struct lets the collector know what method to invoke when a broadcast is received
 static const struct broadcast_callbacks broadcast_call = { broadcast_recv };
 static struct broadcast_conn broadcast;
 
@@ -67,18 +77,33 @@ PROCESS_THREAD(example_unicast_process, ev, data)
     
   PROCESS_BEGIN();
 
+  //Activates the sensor so that we can read the temperature from it
   SENSORS_ACTIVATE(sht11_sensor);
 
+  /*
+  We open a broadcast connection on band 146
+  and open a unicast connection on band 140
+  */ 
   broadcast_open(&broadcast, 146, &broadcast_call);
 
   unicast_open(&uc, 140, &unicast_callbacks);
 
+  //This will be used as the counter for our for loop
   int i = 0;
 
+  /* 
+  We populate the array that holds the moving
+  average for the temperature with 0s
+  */
   for(i = 0; i < 4; i++){
-	values[i] = 0;
+	  values[i] = 0;
   }
 
+  /*
+  This block of code will run until the mote is terminated
+  It periodically takes the temperature and stores it
+  and then calculates the new moving average
+  */
   while(1) {
 
     static struct etimer et;
@@ -91,24 +116,33 @@ PROCESS_THREAD(example_unicast_process, ev, data)
 
     int store = print_val = sht11_sensor.value(SHT11_SENSOR_TEMP);
 
-    
+    /*
+    This block of code pushes out the first value
+    read in out of the array and compresses it leaving 
+    the last index free to add in our new value
+    */
 
     for(i = 0; i < 4; i++){
-	if((i + 1) < 4){
-	  values[i] = values[i + 1];
-
-	}
+	    if((i + 1) < 4){
+	    values[i] = values[i + 1];
+      }
     }
 
+    //We set the last index to our new value
     values[3] = store;
 
     store = 0;
 
-	for(i = 0; i < 4; i++){
-		store = store + values[i];
-	}
+    /*
+    This block of the code gets the sum
+    of the total array
+    */
+	  for(i = 0; i < 4; i++){
+		  store = store + values[i];
+	  }
 
-        print_val = store / 4;
+    //We get the average by diving the sum by four
+    print_val = store / 4;
 
 
 
